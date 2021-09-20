db3ea6ce0f2d26becb413c52b8640632c9cc511a
shashank2043 <83774850+shashank2043@users.noreply.github.com>
2021-09-20 20:49:45 +0530

    Add files via upload

---
+++ Capitalizer.py
@@ -0,0 +1,23 @@
+import lyrics
+def capitalize_each_word(original_str):
+    result = ""
+    # Split the string and get all words in a list
+    list_of_words = original_str.split()
+    # Iterate over all elements in list
+    for elem in list_of_words:
+        # capitalize first letter of each word and add to a string
+        if len(result) > 0:
+            result = result + " " + elem.strip().capitalize()
+        else:
+            result = elem.capitalize()
+    # If result is still empty then return original string else returned capitalized.
+    if not result:
+        return original_str
+    else:
+        return result
+sample_text = lyrics.x1
+result = capitalize_each_word(sample_text)
+f=open("cly.txt", "w")
+f.write(result)
+f.close()
+print(result)
---
+++ lyrics.py
@@ -0,0 +1,4 @@
+x1="Saradaga kaasepina sarijodai neetho unna"
+x2="Saripodaa nakee janmaki"
+x3="Chirunavvai osarina chigurincha lokamlona"
+x4="Idi challe ippudu ee kommaki"
